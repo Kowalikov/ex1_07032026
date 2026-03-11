16231917253d91798ca27e477df972b423bdc03b
marekowa <marek-jozef.kowalik@capgemini.com>
2026-03-11 09:43:01 +0100

    Solved ex3.

---
+++ src/Zad3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+
+public class Zad3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s0 = sc.next();
+        String s1 = sc.next();
+
+        System.out.printf("%%%s %s%%%n", s0, s1);
+    }
+}
