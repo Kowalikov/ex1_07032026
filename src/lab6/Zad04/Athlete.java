c89ee3acc1c998e88ea039df5c6f0a7a5831ba2d
marekowa <marek-jozef.kowalik@capgemini.com>
2026-06-12 23:54:32 +0200

    Solve ex4.

--- src/lab6/Zad04/Athlete.java
+++ src/lab6/Zad04/Athlete.java
@@ -1,6 +1,6 @@
 package lab6.Zad04;
 
-public class Athlete extends Runner{
+public class Athlete implements Runner{
     public void run() {
        System.out.println("Athlete running means, that the athlete is going very fast on their legs, ");
        System.out.println("interchangeably pushing their legs away from the ground. ");
--- src/lab6/Zad04/PoliticalCandidate.java
+++ src/lab6/Zad04/PoliticalCandidate.java
@@ -1,6 +1,6 @@
 package lab6.Zad04;
 
-public class PoliticalCandidate extends Runner{
+public class PoliticalCandidate implements Runner{
     public void run() {
        System.out.println("Running of political candidate means, that they want to be chosen on some political role and ");
        System.out.println("put their candidacy, making campaign and encouraging people to vote on them.");
